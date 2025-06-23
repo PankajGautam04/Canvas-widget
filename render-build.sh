4d4129b1d036df1f786bf2afe4557d8bba974332
Pankaj Gautam <34612449+PankajGautam04@users.noreply.github.com>
2025-06-23 17:59:55 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
-apt-get install -y ffmpeg python3 python3-pip
+apt-get install -y wget ca-certificates gnupg unzip ffmpeg python3 python3-pip chromium chromium-driver
 pip3 install yt-dlp
 npm install
