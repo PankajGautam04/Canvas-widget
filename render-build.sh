a0f830847209ebf6468ec0d9343ce86b8d231b7d
Pankaj Gautam <34612449+PankajGautam04@users.noreply.github.com>
2025-06-24 14:36:54 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Update and install dependencies
 apt-get update && \
 apt-get install -y wget unzip ffmpeg python3 python3-pip fonts-liberation && \
-pip3 install yt-dlp
 
-# Create local directory and install Chromium into it
+# Install yt-dlp
+pip3 install yt-dlp && \
+
+# Set up Chromium manually (for Puppeteer)
 mkdir -p ./chromium
 
 CHROME_URL="https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/1181205/chrome-linux.zip"
 wget -O /tmp/chrome.zip "$CHROME_URL" && \
-unzip /tmp/chrome.zip -d ./chromium && \
+unzip -q /tmp/chrome.zip -d ./chromium && \
 chmod +x ./chromium/chrome-linux/chrome
 
+# Install Node.js dependencies
 npm install
